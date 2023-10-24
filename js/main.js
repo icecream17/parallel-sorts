a38d19b5b8b496955bc6d6efa8641e9c75c7101b
icecream17 <nguyeste008@students.garlandisd.net>
2023-10-24 13:33:18 -0500

    done

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ while (tosort.length) {
   buckets[insertat].push(elem)
 }
 
-// Step 3: Create threads + send each thread a bucket to sort
+// Step 3: Create threads + send each thread a bucket to sort + when all is finish, output
 const threads = []
 let threadsleft = numthreads
 for (let i = 0; i < numthreads; i++) {
@@ -30,48 +30,11 @@ for (let i = 0; i < numthreads; i++) {
   threads[i].onmessage = ({ data }) => {
     threads[i] = data
     threadsleft--
-    if (threadsleft === 0)
-  }
-}
-
-
-///////////////////////
-// Utility functions
-// usually `.sort` would be used
-const sort = array => quicksort(array, 0, array.length - 1)
-
-const quicksort = (array, min, max) => {
-  // done
-  if (min >= max || min < 0) {
-    return
-  }
-
-  // sort into two buckets in place and get pivot index
-  const pivot = partition(array, min, max)
-
-  // sort each bucket
-  quicksort(array, min, pivot - 1)
-  quicksort(array, pivot + 1, max)
-}
-
-const partition = (array, min, max) => {
-  const pivot = array[max]
-
-  let i = min - 1  
-  for (let j = min; j < max; j++) {
-    // move to less-than bucket if less-than
-    if (array[j] < pivot) {
-      i++
-      // swap @ i,j
-      const temp = array[i]
-      array[i] = array[j]
-      array[j] = temp
+    if (threadsleft === 0) {
+      console.log(String(threads)) // taking advantage of Javascript Array toString
     }
   }
-
-  i++
-  // swap @ i,max
-  array[max] = array[i]
-  array[i] = pivot
-  return i
+  threads[i].postMessage(buckets[i])
+  // or:
+  // threads[i].postMessage(undefined, [Uint8Array.from(buckets[i]).buffer])
 }
